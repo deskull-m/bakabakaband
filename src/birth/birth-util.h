353c750f3057c496a7ee75569ee090dc5a151d4d
Hourier <66951241+Hourier@users.noreply.github.com>
2024-10-26 13:15:54 +0900

    [Refactor] #4559 変更範囲のconcptr をstring/string_view に変えた

--- src/birth/birth-util.h
+++ src/birth/birth-util.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "system/angband.h"
+#include <string_view>
 
 enum class BirthKind {
     REALM,
@@ -11,6 +11,6 @@ enum class BirthKind {
 };
 
 class PlayerType;
-void birth_quit(void);
-void show_help(PlayerType *player_ptr, concptr helpfile);
+void birth_quit();
+void show_help(PlayerType *player_ptr, std::string_view helpfile);
 void birth_help_option(PlayerType *player_ptr, char c, BirthKind bk);
