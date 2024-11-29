hengband/hengband

[Refactor] #4503 MonsterRaceInfo::order_level() を実装した

src/room/rooms-nest.cpp
@@ -172,12 +172,9 @@ bool NestMonsterInfo::order_nest(const NestMonsterInfo &other) const
 
     const auto &monrace1 = this->get_monrace();
     const auto &monrace2 = other.get_monrace();
-    if (monrace1.level < monrace2.level) {
-        return true;
-    }
-
-    if (monrace1.level > monrace2.level) {
-        return false;
+    const auto order_level = monrace1.order_level(monrace2);
+    if (order_level) {
+        return *order_level;
     }
 
     if (monrace1.mexp < monrace2.mexp) {

src/system/monster-race-info.cpp
@@ -98,6 +98,19 @@ std::string MonsterRaceInfo::get_died_message() const
     return is_explodable ? _("は爆発して粉々になった。", " explodes into tiny shreds.") : _("を倒した。", " is destroyed.");
 }
 
+std::optional<bool> MonsterRaceInfo::order_level(const MonsterRaceInfo &other) const
+{
+    if (this->level > other.level) {
+        return true;
+    }
+
+    if (this->level < other.level) {
+        return false;
+    }
+
+    return std::nullopt;
+}
+
 std::optional<bool> MonsterRaceInfo::order_pet(const MonsterRaceInfo &other) const
 {
     if (this->kind_flags.has(MonsterKindType::UNIQUE) && other.kind_flags.has_not(MonsterKindType::UNIQUE)) {
@@ -108,15 +121,7 @@ std::optional<bool> MonsterRaceInfo::order_pet(const MonsterRaceInfo &other) con
         return false;
     }
 
-    if (this->level > other.level) {
-        return true;
-    }
-
-    if (this->level < other.level) {
-        return false;
-    }
-
-    return std::nullopt;
+    return this->order_level(other);
 }
 
 /*!
@@ -775,12 +780,9 @@ bool MonraceList::order_level_unique(MonsterRaceId id1, MonsterRaceId id2) const
 {
     const auto &monrace1 = this->get_monrace(id1);
     const auto &monrace2 = this->get_monrace(id2);
-    if (monrace1.level < monrace2.level) {
-        return true;
-    }
-
-    if (monrace1.level > monrace2.level) {
-        return false;
+    const auto order_level = monrace1.order_level(monrace2);
+    if (order_level) {
+        return *order_level;
     }
 
     if (monrace1.kind_flags.has_not(MonsterKindType::UNIQUE) && monrace2.kind_flags.has(MonsterKindType::UNIQUE)) {

deskull-m/bakabakaband

src/room/rooms-nest.cpp
@@ -172,12 +172,9 @@ bool NestMonsterInfo::order_nest(const NestMonsterInfo &other) const
 
     const auto &monrace1 = this->get_monrace();
     const auto &monrace2 = other.get_monrace();
-    if (monrace1.level < monrace2.level) {
-        return true;
-    }
-
-    if (monrace1.level > monrace2.level) {
-        return false;
+    const auto order_level = monrace2.order_level(monrace1);
+    if (order_level) {
+        return *order_level;
     }
 
     if (monrace1.mexp < monrace2.mexp) {

src/system/monster-race-info.cpp
@@ -94,6 +94,19 @@ std::string MonsterRaceInfo::get_died_message() const
     return is_explodable ? _("は爆発して粉々になった。", " explodes into tiny shreds.") : _("を倒した。", " is destroyed.");
 }
 
+std::optional<bool> MonsterRaceInfo::order_level(const MonsterRaceInfo &other) const
+{
+    if (this->level > other.level) {
+        return true;
+    }
+
+    if (this->level < other.level) {
+        return false;
+    }
+
+    return std::nullopt;
+}
+
 std::optional<bool> MonsterRaceInfo::order_pet(const MonsterRaceInfo &other) const
 {
     if (this->kind_flags.has(MonsterKindType::UNIQUE) && other.kind_flags.has_not(MonsterKindType::UNIQUE)) {
@@ -104,15 +117,7 @@ std::optional<bool> MonsterRaceInfo::order_pet(const MonsterRaceInfo &other) con
         return false;
     }
 
-    if (this->level > other.level) {
-        return true;
-    }
-
-    if (this->level < other.level) {
-        return false;
-    }
-
-    return std::nullopt;
+    return this->order_level(other);
 }
 
 /*!
@@ -745,12 +750,9 @@ bool MonraceList::order_level_unique(MonsterRaceId id1, MonsterRaceId id2) const
 {
     const auto &monrace1 = this->get_monrace(id1);
     const auto &monrace2 = this->get_monrace(id2);
-    if (monrace1.level < monrace2.level) {
-        return true;
-    }
-
-    if (monrace1.level > monrace2.level) {
-        return false;
+    const auto order_level = monrace2.order_level(monrace1);
+    if (order_level) {
+        return *order_level;
     }
 
     if (monrace1.kind_flags.has_not(MonsterKindType::UNIQUE) && monrace2.kind_flags.has(MonsterKindType::UNIQUE)) {