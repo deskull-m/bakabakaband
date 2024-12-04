21fd9ed0b88ab025085d48a6f519a6a0ebb5d4ac
Hourier <66951241+Hourier@users.noreply.github.com>
2024-12-04 20:23:24 +0900

    [Refactor] #4689 floor-list.cpp/h をfloor/ からsystem/floor に移した

--- src/birth/game-play-initializer.cpp
+++ src/birth/game-play-initializer.cpp
@@ -21,6 +21,7 @@
 #include "system/dungeon/dungeon-list.h"
 #include "system/dungeon/dungeon-record.h"
 #include "system/floor/floor-info.h"
+#include "system/floor/floor-list.h"
 #include "system/inner-game-data.h"
 #include "system/item-entity.h"
 #include "system/monrace/monrace-definition.h"
