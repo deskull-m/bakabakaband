Hourier/hengband

[Refactor] モンスターの生成禁止フィルタをMonraceAllocEntry 側に定義した

src/system/alloc-entries.cpp
@@ -10,6 +10,45 @@ MonraceAllocationEntry::MonraceAllocationEntry(MonsterRaceId index, int level, s
 {
 }
 
+/*!
+ * @brief 一般的なモンスター生成ルーチンで生成しても良いモンスターか否かのフィルタ処理
+ * @param dungeon_level 生成基準階
+ * @return 生成許可ならばtrue、禁止ならばfalse
+ * @details クエストモンスター、ダンジョンの主、指定階未満でのFORCE_DEPTH フラグ持ちは生成禁止
+ */
+bool MonraceAllocationEntry::is_permitted(int dungeon_level) const
+{
+    const auto &monrace = this->get_monrace();
+    if (monrace.misc_flags.has(MonsterMiscType::QUESTOR)) {
+        return false;
+    }
+
+    if (monrace.misc_flags.has(MonsterMiscType::GUARDIAN)) {
+        return false;
+    }
+
+    if (monrace.misc_flags.has(MonsterMiscType::FORCE_DEPTH) && (monrace.level > dungeon_level)) {
+        return false;
+    }
+
+    return true;
+}
+
+/*!
+ * @brief クエスト内で生成しても良いモンスターか否かのフィルタ処理
+ * @param dungeon_level 生成基準階
+ * @return 生成許可ならばtrue、禁止ならばfalse
+ * @details RES_ALL、または生成標準階未満でのダメージ低減フラグ持ちは生成禁止
+ */
+bool MonraceAllocationEntry::is_defeatable(int dungeon_level) const
+{
+    const auto &monrace = this->get_monrace();
+    const auto has_resist_all = monrace.resistance_flags.has(MonsterResistanceType::RESIST_ALL);
+    const auto can_diminish = monrace.special_flags.has(MonsterSpecialType::DIMINISH_MAX_DAMAGE);
+    const auto is_shallow = monrace.level > dungeon_level;
+    return !has_resist_all && !(can_diminish && is_shallow);
+}
+
 const MonsterRaceInfo &MonraceAllocationEntry::get_monrace() const
 {
     return MonraceList::get_instance().get_monrace(index);

src/system/alloc-entries.h
@@ -20,6 +20,10 @@ public:
     int level; /* Base dungeon level */
     short prob1; /* Probability, pass 1 */
     short prob2; /* Probability, pass 2 */
+    bool is_permitted(int dungeon_level) const;
+    bool is_defeatable(int dungeon_level) const;
+
+private:
     const MonsterRaceInfo &get_monrace() const;
 };
 

deskull-m/bakabakaband

src/system/alloc-entries.cpp
@@ -10,6 +10,45 @@ MonraceAllocationEntry::MonraceAllocationEntry(MonsterRaceId index, int level, s
 {
 }
 
+/*!
+ * @brief 一般的なモンスター生成ルーチンで生成しても良いモンスターか否かのフィルタ処理
+ * @param level 生成基準階
+ * @return 生成許可ならばtrue、禁止ならばfalse
+ * @details クエストモンスター、ダンジョンの主、指定階未満でのFORCE_DEPTH フラグ持ちは生成禁止
+ */
+bool MonraceAllocationEntry::is_permitted(int threshold_level) const
+{
+    const auto &monrace = this->get_monrace();
+    if (monrace.misc_flags.has(MonsterMiscType::QUESTOR)) {
+        return false;
+    }
+
+    if (monrace.misc_flags.has(MonsterMiscType::GUARDIAN)) {
+        return false;
+    }
+
+    if (monrace.misc_flags.has(MonsterMiscType::FORCE_DEPTH) && (monrace.level > threshold_level)) {
+        return false;
+    }
+
+    return true;
+}
+
+/*!
+ * @brief クエスト内で生成しても良いモンスターか否かのフィルタ処理
+ * @param level 生成基準階
+ * @return 生成許可ならばtrue、禁止ならばfalse
+ * @details RES_ALL、または生成標準階未満でのダメージ低減フラグ持ちは生成禁止
+ */
+bool MonraceAllocationEntry::is_defeatable(int threshold_level) const
+{
+    const auto &monrace = this->get_monrace();
+    const auto has_resist_all = monrace.resistance_flags.has(MonsterResistanceType::RESIST_ALL);
+    const auto can_diminish = monrace.special_flags.has(MonsterSpecialType::DIMINISH_MAX_DAMAGE);
+    const auto is_shallow = monrace.level > threshold_level;
+    return !has_resist_all && !(can_diminish && is_shallow);
+}
+
 const MonsterRaceInfo &MonraceAllocationEntry::get_monrace() const
 {
     return MonraceList::get_instance().get_monrace(index);

src/system/alloc-entries.h
@@ -20,6 +20,10 @@ public:
     int level; /* Base dungeon level */
     short prob1; /* Probability, pass 1 */
     short prob2; /* Probability, pass 2 */
+    bool is_permitted(int threshold_level) const;
+    bool is_defeatable(int threshold_level) const;
+
+private:
     const MonsterRaceInfo &get_monrace() const;
 };
 