dbb02b46c5acc5f3caafbfbd00a4e747841e1963
Hourier <66951241+Hourier@users.noreply.github.com>
2024-12-11 22:18:53 +0900

    [Refactor] FloorType::set_terrain_id() に、TerrainTag を引数に取るオーバーロードを追加した

--- src/system/floor/floor-info.h
+++ src/system/floor/floor-info.h
@@ -52,6 +52,7 @@ struct town_vault {
     int transno;
 };
 
+enum class TerrainTag;
 class DungeonDefinition;
 class Grid;
 class MonsterEntity;
@@ -138,6 +139,7 @@ public:
     short pop_empty_index_item();
     bool is_grid_changeable(const Pos2D &pos) const;
     void place_random_stairs(const Pos2D &pos);
+    void set_terrain_id(const Pos2D &pos, TerrainTag tag);
     void set_terrain_id(const Pos2D &pos, short terrain_id);
 
 private:
