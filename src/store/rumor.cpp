eaea92fd903c71830ded2b40e6575ebc8e8e15c8
Hourier <66951241+Hourier@users.noreply.github.com>
2024-12-13 21:13:10 +0900

    [Refactor] max_dlv[] をDungeonRecords に差し替えた

--- src/store/rumor.cpp
+++ src/store/rumor.cpp
@@ -150,8 +150,10 @@ void display_rumor(PlayerType *player_ptr, bool ex)
         }
 
         fullname = d_ptr->name;
-        if (!max_dlv[dungeon_id]) {
-            max_dlv[dungeon_id] = d_ptr->mindepth;
+        auto &dungeon_records = DungeonRecords::get_instance();
+        auto &dungeon_record = dungeon_records.get_record(dungeon_id);
+        if (!dungeon_record.has_entered()) {
+            dungeon_record.set_max_level(d_ptr->mindepth);
             rumor_eff_format = _("%sに帰還できるようになった。", "You can recall to %s.");
         }
     } else if (category == "TOWN") {
