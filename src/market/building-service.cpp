525f63b74d166172873a25663e3aee96db0e10ec
Hourier <66951241+Hourier@users.noreply.github.com>
2024-12-17 19:05:51 +0900

    [Refactor] do_cmd_building() を整形した

--- src/market/building-service.cpp
+++ src/market/building-service.cpp
@@ -15,21 +15,21 @@
  * @param bldg 施設構造体の参照ポインタ
  * @return 種族、職業、魔法領域のいずれかが一致しているかの是非。
  */
-bool is_owner(PlayerType *player_ptr, building_type *bldg)
+bool is_owner(PlayerType *player_ptr, const building_type &bldg)
 {
     constexpr auto building_owner = 2;
-    if (bldg->member_class[enum2i(player_ptr->pclass)] == building_owner) {
+    if (bldg.member_class[enum2i(player_ptr->pclass)] == building_owner) {
         return true;
     }
 
-    if (bldg->member_race[enum2i(player_ptr->prace)] == building_owner) {
+    if (bldg.member_race[enum2i(player_ptr->prace)] == building_owner) {
         return true;
     }
 
     PlayerRealm pr(player_ptr);
     const auto realm1 = pr.realm1().to_enum();
     const auto realm2 = pr.realm2().to_enum();
-    if ((PlayerRealm::is_magic(realm1) && (bldg->member_realm[enum2i(realm1)] == building_owner)) || (PlayerRealm::is_magic(realm2) && (bldg->member_realm[enum2i(realm2)] == building_owner))) {
+    if ((PlayerRealm::is_magic(realm1) && (bldg.member_realm[enum2i(realm1)] == building_owner)) || (PlayerRealm::is_magic(realm2) && (bldg.member_realm[enum2i(realm2)] == building_owner))) {
         return true;
     }
 
@@ -46,20 +46,20 @@ bool is_owner(PlayerType *player_ptr, building_type *bldg)
  * @return 種族、職業、魔法領域のいずれかが一致しているかの是非。
  * @todo is_owner()との実質的な多重実装なので、リファクタリングを行うべきである。
  */
-bool is_member(PlayerType *player_ptr, building_type *bldg)
+bool is_member(PlayerType *player_ptr, const building_type &bldg)
 {
-    if (static_cast<bool>(bldg->member_class[enum2i(player_ptr->pclass)])) {
+    if (static_cast<bool>(bldg.member_class[enum2i(player_ptr->pclass)])) {
         return true;
     }
 
-    if (static_cast<bool>(bldg->member_race[enum2i(player_ptr->prace)])) {
+    if (static_cast<bool>(bldg.member_race[enum2i(player_ptr->prace)])) {
         return true;
     }
 
     PlayerRealm pr(player_ptr);
     const auto realm1 = pr.realm1().to_enum();
     const auto realm2 = pr.realm2().to_enum();
-    if ((PlayerRealm::is_magic(realm1) && bldg->member_realm[enum2i(realm1)]) || (PlayerRealm::is_magic(realm2) && bldg->member_realm[enum2i(realm2)])) {
+    if ((PlayerRealm::is_magic(realm1) && bldg.member_realm[enum2i(realm1)]) || (PlayerRealm::is_magic(realm2) && bldg.member_realm[enum2i(realm2)])) {
         return true;
     }
 
@@ -68,7 +68,7 @@ bool is_member(PlayerType *player_ptr, building_type *bldg)
     }
 
     for (int i = 0; i < MAX_MAGIC; i++) {
-        if (bldg->member_realm[i + 1]) {
+        if (bldg.member_realm[i + 1]) {
             return true;
         }
     }
@@ -81,71 +81,66 @@ bool is_member(PlayerType *player_ptr, building_type *bldg)
  * @param player_ptr プレイヤーへの参照ポインタ
  * @param bldg 施設構造体の参照ポインタ
  */
-void display_buikding_service(PlayerType *player_ptr, building_type *bldg)
+void display_buikding_service(PlayerType *player_ptr, const building_type &bldg)
 {
-    char buff[20];
     byte action_color;
-    char tmp_str[80];
 
     term_clear();
-    sprintf(tmp_str, "%s (%s) %35s", bldg->owner_name, bldg->owner_race, bldg->name);
-    prt(tmp_str, 2, 1);
+    prt(format("%s (%s) %35s", bldg.owner_name, bldg.owner_race, bldg.name), 2, 1);
 
     for (int i = 0; i < 8; i++) {
-        if (!bldg->letters[i]) {
+        if (!bldg.letters[i]) {
             continue;
         }
 
-        if (bldg->action_restr[i] == 0) {
-            if ((is_owner(player_ptr, bldg) && (bldg->member_costs[i] == 0)) || (!is_owner(player_ptr, bldg) && (bldg->other_costs[i] == 0))) {
+        std::string buff;
+        if (bldg.action_restr[i] == 0) {
+            if ((is_owner(player_ptr, bldg) && (bldg.member_costs[i] == 0)) || (!is_owner(player_ptr, bldg) && (bldg.other_costs[i] == 0))) {
                 action_color = TERM_WHITE;
                 buff[0] = '\0';
             } else if (is_owner(player_ptr, bldg)) {
                 action_color = TERM_YELLOW;
-                sprintf(buff, _("($%ld)", "(%ldgp)"), (long int)bldg->member_costs[i]);
+                buff = format(_("($%d)", "(%dgp)"), bldg.member_costs[i]);
             } else {
                 action_color = TERM_YELLOW;
-                sprintf(buff, _("($%ld)", "(%ldgp)"), (long int)bldg->other_costs[i]);
+                buff = format(_("($%d)", "(%dgp)"), bldg.other_costs[i]);
             }
 
-            sprintf(tmp_str, " %c) %s %s", bldg->letters[i], bldg->act_names[i], buff);
-            c_put_str(action_color, tmp_str, 19 + (i / 2), 35 * (i % 2));
+            c_put_str(action_color, format(" %c) %s %s", bldg.letters[i], bldg.act_names[i], buff.data()), 19 + (i / 2), 35 * (i % 2));
             continue;
         }
 
-        if (bldg->action_restr[i] == 1) {
+        if (bldg.action_restr[i] == 1) {
             if (!is_member(player_ptr, bldg)) {
                 action_color = TERM_L_DARK;
-                strcpy(buff, _("(閉店)", "(closed)"));
-            } else if ((is_owner(player_ptr, bldg) && (bldg->member_costs[i] == 0)) || (is_member(player_ptr, bldg) && (bldg->other_costs[i] == 0))) {
+                buff = _("(閉店)", "(closed)");
+            } else if ((is_owner(player_ptr, bldg) && (bldg.member_costs[i] == 0)) || (is_member(player_ptr, bldg) && (bldg.other_costs[i] == 0))) {
                 action_color = TERM_WHITE;
                 buff[0] = '\0';
             } else if (is_owner(player_ptr, bldg)) {
                 action_color = TERM_YELLOW;
-                sprintf(buff, _("($%ld)", "(%ldgp)"), (long int)bldg->member_costs[i]);
+                buff = format(_("($%d)", "(%dgp)"), bldg.member_costs[i]);
             } else {
                 action_color = TERM_YELLOW;
-                sprintf(buff, _("($%ld)", "(%ldgp)"), (long int)bldg->other_costs[i]);
+                buff = format(_("($%d)", "(%dgp)"), bldg.other_costs[i]);
             }
 
-            sprintf(tmp_str, " %c) %s %s", bldg->letters[i], bldg->act_names[i], buff);
-            c_put_str(action_color, tmp_str, 19 + (i / 2), 35 * (i % 2));
+            c_put_str(action_color, format(" %c) %s %s", bldg.letters[i], bldg.act_names[i], buff.data()), 19 + (i / 2), 35 * (i % 2));
             continue;
         }
 
         if (!is_owner(player_ptr, bldg)) {
             action_color = TERM_L_DARK;
-            strcpy(buff, _("(閉店)", "(closed)"));
-        } else if (bldg->member_costs[i] != 0) {
+            buff = _("(閉店)", "(closed)");
+        } else if (bldg.member_costs[i] != 0) {
             action_color = TERM_YELLOW;
-            sprintf(buff, _("($%ld)", "(%ldgp)"), (long int)bldg->member_costs[i]);
+            buff = format(_("($%d)", "(%dgp)"), bldg.member_costs[i]);
         } else {
             action_color = TERM_WHITE;
             buff[0] = '\0';
         }
 
-        sprintf(tmp_str, " %c) %s %s", bldg->letters[i], bldg->act_names[i], buff);
-        c_put_str(action_color, tmp_str, 19 + (i / 2), 35 * (i % 2));
+        c_put_str(action_color, format(" %c) %s %s", bldg.letters[i], bldg.act_names[i], buff.data()), 19 + (i / 2), 35 * (i % 2));
     }
 
     prt(_(" ESC) 建物を出る", " ESC) Exit building"), 23, 0);
