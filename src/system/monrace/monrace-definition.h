320645f12272dcef4823d789bad89168bb1ad9ad
Hourier <66951241+Hourier@users.noreply.github.com>
2024-12-29 20:37:30 +0900

    [Refactor] MonraceDefinition::is_suitable_for_trapped_pit() を実装し、vault_aux_trapped_pit() を解体した

--- src/system/monrace/monrace-definition.h
+++ src/system/monrace/monrace-definition.h
@@ -197,6 +197,7 @@ public:
     bool is_suitable_for_shallow_water() const;
     bool is_suitable_for_deep_water() const;
     bool is_suitable_for_lava() const;
+    bool is_suitable_for_trapped_pit() const;
 
     void init_sex(uint32_t value);
 
