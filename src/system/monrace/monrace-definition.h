5e85869838b738a3d3f64b33c371588d69780350
Hourier <66951241+Hourier@users.noreply.github.com>
2024-12-29 19:51:21 +0900

    [Refactor] MonraceDefinition::is_suitable_for_lava() を実装してmon_hook_lava() を解体した

--- src/system/monrace/monrace-definition.h
+++ src/system/monrace/monrace-definition.h
@@ -195,6 +195,7 @@ public:
     bool is_suitable_for_random_quest() const;
     bool is_suitable_for_shallow_water() const;
     bool is_suitable_for_deep_water() const;
+    bool is_suitable_for_lava() const;
 
     void init_sex(uint32_t value);
 
