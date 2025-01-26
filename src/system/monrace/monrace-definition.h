f03362947d4995d09ec7d32b79a0c01b57297bf8
Hourier <66951241+Hourier@users.noreply.github.com>
2025-01-26 11:23:29 +0900

    [Refactor] MonraceDefinition::is_suitable_for_good_nest() を実装してvault_aux_symbol_g() を解体した

--- src/system/monrace/monrace-definition.h
+++ src/system/monrace/monrace-definition.h
@@ -228,6 +228,8 @@ public:
     bool is_suitable_for_jelly_nest() const;
     bool is_suitable_for_animal_nest() const;
     bool is_suitable_for_undead_nest() const;
+    bool is_suitable_for_dragon_nest(const EnumClassFlagGroup<MonsterAbilityType> &dragon_breaths) const;
+    bool is_suitable_for_good_nest(char symbol) const;
 
     void init_sex(uint32_t value);
 
