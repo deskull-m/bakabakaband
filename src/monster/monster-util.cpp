1301159eb6a61650fd580c815d75f144730b8f7d
Hourier <66951241+Hourier@users.noreply.github.com>
2025-01-31 21:48:34 +0900

    [Refactor] wt_type をsystem/enums/terrain/wilderness-terrain.h へ移した

--- src/monster/monster-util.cpp
+++ src/monster/monster-util.cpp
@@ -16,6 +16,7 @@
 #include "system/angband-system.h"
 #include "system/dungeon/dungeon-definition.h"
 #include "system/enums/monrace/monrace-id.h"
+#include "system/enums/terrain/wilderness-terrain.h"
 #include "system/floor/floor-info.h"
 #include "system/grid-type-definition.h"
 #include "system/monrace/monrace-allocation.h"
