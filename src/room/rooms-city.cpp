5fe4a2de635706a6dd149ebfb0848e3f0ec0fd69
Hourier <66951241+Hourier@users.noreply.github.com>
2025-03-13 11:38:11 +0900

    [Refactor] system/terrain-type-definition.cpp/h をsystem/terrain/terrain-definition.cpp/h に移した

--- src/room/rooms-city.cpp
+++ src/room/rooms-city.cpp
@@ -13,7 +13,7 @@
 #include "system/grid-type-definition.h"
 #include "system/item-entity.h"
 #include "system/player-type-definition.h"
-#include "system/terrain-type-definition.h"
+#include "system/terrain/terrain-definition.h"
 #include "util/bit-flags-calculator.h"
 #include "wizard/wizard-messages.h"
 #include <algorithm>
