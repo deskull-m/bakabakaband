hengband/hengband

[Fix] 闘技場の地形が正しく生成されない
342d04c8 でのエンバグ。

src/floor/floor-generator.cpp
@@ -236,9 +236,8 @@ static void generate_gambling_arena(PlayerType *player_ptr)
         }
     }
 
-    Pos2D pos_arena(1, 1);
-    for (; pos_arena.y < SCREEN_HGT - 1; pos_arena.y++) {
-        for (; pos_arena.x < SCREEN_WID - 1; pos_arena.x++) {
+    for (Pos2D pos_arena(1, 1); pos_arena.y < SCREEN_HGT - 1; pos_arena.y++) {
+        for (pos_arena.x = 1; pos_arena.x < SCREEN_WID - 1; pos_arena.x++) {
             floor.get_grid(pos_arena).set_terrain_id(TerrainTag::FLOOR);
         }
     }

deskull-m/bakabakaband

src/floor/floor-generator.cpp
@@ -247,9 +247,8 @@ static void generate_gambling_arena(PlayerType *player_ptr)
         }
     }
 
-    Pos2D pos_arena(1, 1);
-    for (; pos_arena.y < ARENA_HGT - 1; pos_arena.y++) {
-        for (; pos_arena.x < ARENA_WID - 1; pos_arena.x++) {
+    for (Pos2D pos_arena(1, 1); pos_arena.y < ARENA_HGT - 1; pos_arena.y++) {
+        for (pos_arena.x = 1; pos_arena.x < ARENA_WID - 1; pos_arena.x++) {
             floor.get_grid(pos_arena).set_terrain_id(TerrainTag::FLOOR);
         }
     }