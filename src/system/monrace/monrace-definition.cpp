hengband/hengband

[Refactor] MonraceDefinition::can_generate()
メソッドの名称と実際に判定している事が逆になっているので、名称に合わせた
判定に修正し、呼び出し側で結果の評価を反転する。

src/system/monrace/monrace-definition.cpp
@@ -328,8 +328,8 @@ const std::vector<Reinforce> &MonraceDefinition::get_reinforces() const
 
 bool MonraceDefinition::can_generate() const
 {
-    auto can_generate = this->kind_flags.has(MonsterKindType::UNIQUE) || this->population_flags.has(MonsterPopulationType::NAZGUL);
-    can_generate &= this->cur_num >= this->max_num;
+    auto can_generate = this->kind_flags.has_not(MonsterKindType::UNIQUE) && this->population_flags.has_not(MonsterPopulationType::NAZGUL);
+    can_generate |= this->cur_num < this->max_num;
     return can_generate;
 }
 

deskull-m/bakabakaband

src/system/monrace/monrace-definition.cpp
@@ -322,8 +322,8 @@ const std::vector<Reinforce> &MonraceDefinition::get_reinforces() const
 
 bool MonraceDefinition::can_generate() const
 {
-    auto can_generate = this->kind_flags.has(MonsterKindType::UNIQUE) || this->population_flags.has(MonsterPopulationType::NAZGUL);
-    can_generate &= this->cur_num >= this->mob_num;
+    auto can_generate = this->kind_flags.has_not(MonsterKindType::UNIQUE) && this->population_flags.has_not(MonsterPopulationType::NAZGUL);
+    can_generate |= this->cur_num < this->mob_num;
     return can_generate;
 }
 