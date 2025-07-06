7e2a5037df2482a1e00125e39ded3183bc84923a
Deskull <sikabane-works@users.noreply.github.com>
2025-07-06 15:49:45 +0900

    (Alliance)[Update] 牙一族の報復処理追加。

--- src/alliance/alliance-fangfamily.cpp
+++ src/alliance/alliance-fangfamily.cpp
@@ -31,3 +31,37 @@ bool AllianceFangFamily::isAnnihilated()
 {
     return MonraceList::get_instance().get_monrace(MonraceId::KING_FANG_FAMILY).mob_num == 0;
 }
+
+void AllianceFangFamily::panishment(PlayerType &player_ptr)
+{
+    auto impression = calcImpressionPoint(&player_ptr);
+    if (isAnnihilated() || impression > -30) {
+        return;
+    }
+
+    if (one_in_(25)) {
+        Pos2D m_pos(player_ptr.get_position());
+        m_pos = scatter(&player_ptr, m_pos, 10, PROJECT_NONE);
+        MonraceId avenger_id;
+        if (impression < -200) {
+            avenger_id = MonraceId::KING_FANG_FAMILY;
+        } else {
+            avenger_id = MonraceId::FANG_FAMILY;
+        }
+        const auto m_idx = place_monster_one(&player_ptr, m_pos.y, m_pos.x, avenger_id, PM_ALLOW_GROUP);
+        if (m_idx) {
+            if (avenger_id == MonraceId::KING_FANG_FAMILY) {
+                msg_print(_("「一族の復讐を受けよ！」族長があなたを始末しに現れた！",
+                    "\"Feel the revenge of Fang Family!\" The chief appears to eliminate you!"));
+            } else {
+                msg_print(_("「華山群狼拳！」牙一族があなたに報復してきた！",
+                    "\"For the honor of Fang Family!\" A Fang family member is chasing you for revenge!"));
+            }
+
+            disturb(&player_ptr, true, true);
+            for (int k = 0; k < 4; k++) {
+                summon_specific(&player_ptr, m_pos.y, m_pos.x, std::max(player_ptr.current_floor_ptr->monster_level, 5), SUMMON_ALLIANCE, PM_ALLOW_GROUP, m_idx);
+            }
+        }
+    }
+}
--- src/alliance/alliance-fangfamily.h
+++ src/alliance/alliance-fangfamily.h
@@ -8,6 +8,7 @@ public:
     AllianceFangFamily() = delete;
     EnumClassFlagGroup<alliance_flags> alliFlags; //!< 陣営特性フラグ
     int calcImpressionPoint(PlayerType *creature_ptr) const override;
+    void panishment(PlayerType &player_ptr) override;
     bool isAnnihilated() override;
     virtual ~AllianceFangFamily() = default;
 };
