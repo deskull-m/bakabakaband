87d2c0413e4c0cd58e013bd8050876c9ec50020f
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-08-24 21:29:09 +0900

    (Monster)[Add] アライアンス制限モンスター生成フラグ `PM_ALLIANCE_LIMIT` を追加。

--- src/monster/monster-list.h
+++ src/monster/monster-list.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "alliance/alliance.h"
 #include <cstdint>
 #include <tl/optional.hpp>
 
@@ -7,6 +8,6 @@ enum class MonraceId : int16_t;
 class FloorType;
 class MonraceDefinition;
 class PlayerType;
-MonraceId get_mon_num(PlayerType *player_ptr, int min_level, int max_level, uint32_t mode);
+MonraceId get_mon_num(PlayerType *player_ptr, int min_level, int max_level, uint32_t mode, AllianceType alliance_type = AllianceType::NONE);
 void choose_chameleon_polymorph(PlayerType *player_ptr, short m_idx, short terrain_id, tl::optional<short> summoner_m_idx = tl::nullopt);
 int get_monster_crowd_number(const FloorType &floor, short m_idx);
