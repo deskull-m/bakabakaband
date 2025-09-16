462d0ac83f101ce041024da7483dd6a7608786de
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-09-16 18:15:01 +0900

    (Command)[Update] 体当たりコマンド実装

--- src/cmd-action/cmd-attack.h
+++ src/cmd-action/cmd-attack.h
@@ -6,3 +6,4 @@
 class PlayerType;
 bool do_cmd_attack(PlayerType *player_ptr, POSITION y, POSITION x, combat_options mode);
 bool do_cmd_headbutt(PlayerType *player_ptr);
+void do_cmd_body_slam(PlayerType *player_ptr);
