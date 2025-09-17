e0a19c125b367b82d616430ee88fd2211db3b815
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-09-17 17:59:30 +0900

    (Terrain)[Update] 片方向ポータル機能の実装。

--- src/cmd-action/cmd-move.h
+++ src/cmd-action/cmd-move.h
@@ -3,6 +3,7 @@
 class PlayerType;
 void do_cmd_go_up(PlayerType *player_ptr);
 void do_cmd_go_down(PlayerType *player_ptr);
+void do_cmd_go_portal(PlayerType *player_ptr);
 void do_cmd_walk(PlayerType *player_ptr, bool pickup);
 void do_cmd_run(PlayerType *player_ptr);
 void do_cmd_stay(PlayerType *player_ptr, bool pickup);
