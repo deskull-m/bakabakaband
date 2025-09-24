bf974c7a4fe5f5e8ec30f1ec791bc0212a1c8b4e
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-09-25 08:47:01 +0900

    (Alliance)[Add] 新アライアンス「聖帝軍」

---
+++ src/alliance/alliance-seitei.cpp
@@ -0,0 +1,22 @@
+#include "alliance/alliance-seitei.h"
+#include "system/enums/monrace/monrace-id.h"
+#include "system/monrace/monrace-definition.h"
+#include "system/monrace/monrace-list.h"
+#include "system/player-type-definition.h"
+
+int AllianceSEITEI::calcImpressionPoint([[maybe_unused]] PlayerType *creature_ptr) const
+{
+    int impression = 0;
+    impression += Alliance::calcPlayerPower(*creature_ptr, 15, 15);
+
+    // 聖帝軍アライアンスのモンスター撃破による印象値低下
+    // TODO: 聖帝軍関連モンスター実装時に追加
+
+    return impression;
+}
+
+bool AllianceSEITEI::isAnnihilated()
+{
+    // TODO: 聖帝サウザー実装時に条件を追加
+    return false;
+}
