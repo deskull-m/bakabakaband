4c9856ad1e7c6e8184458afb54caf4547d4c1cb3
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-10-16 08:55:01 +0900

    (Alliance)[Update] アライアンス「ウトゥムノ」の壊滅条件にメルコール撃破を追加。

--- src/alliance/alliance-utumno.h
+++ src/alliance/alliance-utumno.h
@@ -8,4 +8,5 @@ class AllianceUtumno : public Alliance {
 public:
     AllianceUtumno(AllianceType id, std::string tag, std::string name, int64_t base_power);
     int calcImpressionPoint(PlayerType *creature_ptr) const override;
+    bool isAnnihilated() override;
 };
