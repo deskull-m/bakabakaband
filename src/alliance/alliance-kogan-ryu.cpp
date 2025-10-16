1e8a04ddbac6de2f6518c9174136cf6843d3d7a7
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-10-17 00:12:05 +0900

    (Alliance)[Update] アライアンス「虎眼流」の壊滅条件に岩本虎眼撃破を追加。

--- src/alliance/alliance-kogan-ryu.cpp
+++ src/alliance/alliance-kogan-ryu.cpp
@@ -1,4 +1,7 @@
 #include "alliance/alliance-kogan-ryu.h"
+#include "system/enums/monrace/monrace-id.h"
+#include "system/monrace/monrace-definition.h"
+#include "system/monrace/monrace-list.h"
 #include "system/player-type-definition.h"
 
 int AllianceKoganRyu::calcImpressionPoint(PlayerType *creature_ptr) const
@@ -6,4 +9,15 @@ int AllianceKoganRyu::calcImpressionPoint(PlayerType *creature_ptr) const
     int impression = 0;
     impression += Alliance::calcPlayerPower(*creature_ptr, 15, 18);
     return impression;
+}
+
+/*!
+ * @brief 虎眼流のアライアンスが壊滅したかどうか判定する
+ * @return 壊滅フラグ
+ * @details 濃尾無双『岩本虎眼』が存在しない場合に壊滅する
+ */
+bool AllianceKoganRyu::isAnnihilated()
+{
+    // 濃尾無双『岩本虎眼』が存在しない場合、虎眼流は壊滅する
+    return MonraceList::get_instance().get_monrace(MonraceId::IWAMOTO_KOGAN).cur_num == 0;
 }
