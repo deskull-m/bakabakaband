7ba3a47fe331c0aef7a4a2fe1456d3ef10613b46
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-11-12 12:35:03 +0900

    (Allinace)[Update] アライアンス「アーリアン・ファミリー」の印象地計算修正。

--- src/alliance/alliance-aryan-family.cpp
+++ src/alliance/alliance-aryan-family.cpp
@@ -1,4 +1,5 @@
 #include "alliance/alliance-aryan-family.h"
+#include "alliance/alliance.h"
 #include "system/enums/monrace/monrace-id.h"
 #include "system/monrace/monrace-definition.h"
 #include "system/monrace/monrace-list.h"
@@ -8,6 +9,31 @@ int AllianceAryanFamily::calcImpressionPoint(PlayerType *creature_ptr) const
 {
     int impression = 0;
     impression += Alliance::calcPlayerPower(*creature_ptr, 14, 22);
+
+    // アーリアン・ファミリーのアライアンス所属モンスター撃破による印象値減少
+    const std::string aryan_family_tag = get_alliance_type_tag(AllianceType::ARYAN_FAMILY);
+    const std::string alliance_kill_key = "KILL/ALLIANCE/" + aryan_family_tag;
+    if (creature_ptr->incident_tree.count(alliance_kill_key)) {
+        impression -= creature_ptr->incident_tree.at(alliance_kill_key) * 15; // 1体につき-15
+    }
+
+    // 個別モンスター撃破による追加印象値減少
+    const auto &monrace_list = MonraceList::get_instance();
+
+    // ボス『ビッグ・アイ』撃破による大幅減点
+    if (monrace_list.get_monrace(MonraceId::BIG_EYE).r_pkills > 0) {
+        impression -= 500; // ボス撃破による大幅ペナルティ
+    }
+
+    // 悲しみのレイベン撃破による減点
+    impression -= monrace_list.get_monrace(MonraceId::RAVEN_THE_GRIEF).r_pkills * 200;
+
+    // コモドドラゴン撃破による減点
+    impression -= monrace_list.get_monrace(MonraceId::KOMODO_DRAGON).r_pkills * 10;
+
+    // アーリアン・ファミリーの構成員撃破による減点
+    impression -= monrace_list.get_monrace(MonraceId::ARYAN_FAMILY_MEMBER).r_pkills * 5;
+
     return impression;
 }
 
