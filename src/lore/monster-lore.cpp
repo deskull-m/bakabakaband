0372ea78f6230d96315c56dbef37522663d8f584
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-11-13 08:55:42 +0900

    (Monster)[Update] モンスター種族にビーストを追加

--- src/lore/monster-lore.cpp
+++ src/lore/monster-lore.cpp
@@ -310,6 +310,10 @@ static void set_race_flags(lore_type *lore_ptr)
         lore_ptr->kind_flags.set(MonsterKindType::NOBLE);
     }
 
+    if (lore_ptr->r_ptr->kind_flags.has(MonsterKindType::BEAST)) {
+        lore_ptr->kind_flags.set(MonsterKindType::BEAST);
+    }
+
     if (lore_ptr->r_ptr->kind_flags.has(MonsterKindType::APE)) {
         lore_ptr->kind_flags.set(MonsterKindType::APE);
     }
