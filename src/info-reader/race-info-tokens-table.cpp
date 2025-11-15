1c415516159bf56671e7b277fbfb8b6cdd396cff
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-11-15 17:00:29 +0900

    (Monster)[Update] モンスター種族に「蠍」を追加。

--- src/info-reader/race-info-tokens-table.cpp
+++ src/info-reader/race-info-tokens-table.cpp
@@ -449,6 +449,7 @@ const std::unordered_map<std::string_view, MonsterKindType> r_info_kind_flags = 
     { "TREEFOLK", MonsterKindType::TREEFOLK },
     { "VIRUS", MonsterKindType::VIRUS },
     { "SPHINX", MonsterKindType::SPHINX },
+    { "SCORPION", MonsterKindType::SCORPION },
     { "MONKEY_SPACE", MonsterKindType::MONKEY_SPACE }, // 猿空間
     { "APE", MonsterKindType::APE }, // 類人猿
     { "HORSE", MonsterKindType::HORSE }, // 馬
--- src/lore/monster-lore.cpp
+++ src/lore/monster-lore.cpp
@@ -338,6 +338,10 @@ static void set_race_flags(lore_type *lore_ptr)
         lore_ptr->kind_flags.set(MonsterKindType::SPHINX);
     }
 
+    if (lore_ptr->r_ptr->kind_flags.has(MonsterKindType::SCORPION)) {
+        lore_ptr->kind_flags.set(MonsterKindType::SCORPION);
+    }
+
     if (lore_ptr->r_ptr->kind_flags.has(MonsterKindType::APE)) {
         lore_ptr->kind_flags.set(MonsterKindType::APE);
     }
