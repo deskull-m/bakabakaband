7040534af4bba1e12640e307d31caf72a2723aaf
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2025-12-13 20:43:17 +0900

    (Alliance)[Update] エルドラージ/オディオ/ファイレクシアは自己アライアンス以外のモンスターを全て敵対関係とする。

--- src/alliance/alliance-eldrazi.cpp
+++ src/alliance/alliance-eldrazi.cpp
@@ -1,5 +1,6 @@
 #include "alliance/alliance-eldrazi.h"
 #include "game-option/birth-options.h"
+#include "system/monrace/monrace-definition.h"
 #include "system/player-type-definition.h"
 
 int AllianceEldrazi::calcImpressionPoint([[maybe_unused]] PlayerType *creature_ptr) const
@@ -11,4 +12,16 @@ int AllianceEldrazi::calcImpressionPoint([[maybe_unused]] PlayerType *creature_p
     }
 
     return impression;
+}
+
+/*!
+ * @brief エルドラージは同アライアンス以外の全てのモンスターを敵対対象とする
+ * @param monster_other 敵意を抱くか調べる他のモンスターの参照
+ * @param monrace モンスター種族情報の参照
+ * @return 同アライアンスでなければtrue
+ */
+bool AllianceEldrazi::is_hostile_to([[maybe_unused]] const MonsterEntity &monster_other, const MonraceDefinition &monrace) const
+{
+    // 同アライアンス以外は全て敵
+    return monrace.alliance_idx != AllianceType::ELDRAZI;
 }
--- src/alliance/alliance-odio.h
+++ src/alliance/alliance-odio.h
@@ -6,5 +6,6 @@ public:
     AllianceOdio() = delete;
     EnumClassFlagGroup<alliance_flags> alliFlags; //!< 陣営特性フラグ
     int calcImpressionPoint(PlayerType *creature_ptr) const override;
+    bool is_hostile_to(const MonsterEntity &monster_other, const MonraceDefinition &monrace) const override;
     virtual ~AllianceOdio() = default;
 };
