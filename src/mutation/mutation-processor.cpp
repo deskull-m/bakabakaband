hengband/hengband

[Fix] 各種一時効果が切れる時にトラベルを停止する
例外を作ると面倒なので、一時効果が切れる時は全てトラベルを停止するようにしました。

src/mutation/mutation-processor.cpp
@@ -71,7 +71,7 @@ void process_world_aux_mutation(PlayerType *player_ptr)
         disturb(player_ptr, false, true);
         msg_print(_("ウガァァア！", "RAAAAGHH!"));
         msg_print(_("激怒の発作に襲われた！", "You feel a fit of rage coming over you!"));
-        (void)set_shero(player_ptr, 10 + randint1(player_ptr->lev), false);
+        (void)set_berserk(player_ptr, 10 + randint1(player_ptr->lev), false);
         (void)bss.set_fear(0);
     }
 

deskull-m/bakabakaband

src/mutation/mutation-processor.cpp
@@ -112,7 +112,7 @@ void process_world_aux_mutation(PlayerType *player_ptr)
         disturb(player_ptr, false, true);
         msg_print(_("ウガァァア！", "RAAAAGHH!"));
         msg_print(_("激怒の発作に襲われた！", "You feel a fit of rage coming over you!"));
-        (void)set_shero(player_ptr, 10 + randint1(player_ptr->level), false);
+        (void)set_berserk(player_ptr, 10 + randint1(player_ptr->level), false);
         (void)bss.set_fear(0);
     }
 
@@ -202,7 +202,7 @@ void process_world_aux_mutation(PlayerType *player_ptr)
         RedrawingFlagsUpdater::get_instance().set_flags(flags);
 
         (void)bss.mod_hallucination(randint0(10) + 20);
-        (void)set_shero(player_ptr, 10 + randint1(player_ptr->level), false);
+        (void)set_berserk(player_ptr, 10 + randint1(player_ptr->level), false);
         (void)set_acceleration(player_ptr, 10 + randint1(player_ptr->level), false);
     }
 