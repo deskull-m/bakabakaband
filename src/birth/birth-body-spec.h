817705b387294d46d523de8892f50c674ac9f36b
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-01-21 12:33:09 +0900

    (System)[Refactor] `src/birth/birth-body-spec.cpp` とそれに関わる関数の第一引数を`PlayerType *player_ptr` から `CreatureEntity &creature` に置換

--- src/birth/birth-body-spec.h
+++ src/birth/birth-body-spec.h
@@ -1,7 +1,6 @@
 #pragma once
 
-class PlayerType;
 class CreatureEntity;
 void get_height_weight(CreatureEntity *creature_ptr);
-void get_ahw(PlayerType *player_ptr);
-void get_money(PlayerType *player_ptr);
+void get_ahw(CreatureEntity &creature);
+void get_money(CreatureEntity &creature);
