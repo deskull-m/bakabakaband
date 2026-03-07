4cbd40d3f0de310785061eb77a68c4036a28dfaa
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-03-07 16:27:27 +0900

    refactor: knowledge/knowledge-alliance関数をCreatureEntity&に変更

--- src/knowledge/knowledge-alliance.cpp
+++ src/knowledge/knowledge-alliance.cpp
@@ -7,14 +7,14 @@
 #include "alliance/alliance.h"
 #include "core/show-file.h"
 #include "io-dump/dump-util.h"
+#include "system/creature-entity.h"
 #include "system/monrace/monrace-definition.h"
 #include "system/monrace/monrace-list.h"
-#include "system/player-type-definition.h"
 #include "util/angband-files.h"
 #include <sstream>
 #include <string>
 
-void do_cmd_knowledge_alliance(PlayerType *player_ptr, bool detail)
+void do_cmd_knowledge_alliance(CreatureEntity &creature, bool detail)
 {
     FILE *fff = NULL;
     GAME_TEXT file_name[FILE_NAME_SIZE];
@@ -35,7 +35,7 @@ void do_cmd_knowledge_alliance(PlayerType *player_ptr, bool detail)
         st << a.second->calcCurrentPower();
 
         if (detail) {
-            fprintf(fff, _("%-30s/ あなたへの印象値: %+5d \n", "%-30s/ Impression to you: %+5d \n"), a.second->name.c_str(), a.second->calcImpressionPoint(*player_ptr));
+            fprintf(fff, _("%-30s/ あなたへの印象値: %+5d \n", "%-30s/ Impression to you: %+5d \n"), a.second->name.c_str(), a.second->calcImpressionPoint(creature));
             fprintf(fff, _("  勢力指数: %s \n", "  Power Value: %s \n"), st.str().c_str());
             fprintf(fff, _("  (%s) \n", "  (%s) \n"), (a.second->isAnnihilated() ? _("壊滅", "Annihilated") : _("健在", "Alive")));
 
@@ -69,11 +69,11 @@ void do_cmd_knowledge_alliance(PlayerType *player_ptr, bool detail)
             }
             fprintf(fff, "\n\n");
         } else {
-            fprintf(fff, "%s %-30s %+6d %12s\n", (a.second->isAnnihilated() ? _("壊滅", "Annihilated") : _("健在", "Alive")), a.second->name.c_str(), a.second->calcImpressionPoint(*player_ptr), st.str().c_str());
+            fprintf(fff, "%s %-30s %+6d %12s\n", (a.second->isAnnihilated() ? _("壊滅", "Annihilated") : _("健在", "Alive")), a.second->name.c_str(), a.second->calcImpressionPoint(creature), st.str().c_str());
         }
     }
 
     angband_fclose(fff);
-    FileDisplayer(player_ptr->name).display(true, file_name, 0, 0, _("各アライアンス情報", "Information of all alliances"));
+    FileDisplayer(creature.name).display(true, file_name, 0, 0, _("各アライアンス情報", "Information of all alliances"));
     fd_kill(file_name);
 }
