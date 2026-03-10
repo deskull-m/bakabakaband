20bc6268e3cd84c88e31986a455cf1dfdb5ab46c
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-03-10 22:27:18 +0900

    refactor: store/pricing::price_itemをCreatureEntity&に変更

--- src/store/purchase-order.cpp
+++ src/store/purchase-order.cpp
@@ -46,7 +46,7 @@
  */
 static tl::optional<PRICE> prompt_to_buy(PlayerType *player_ptr, ItemEntity *o_ptr, StoreSaleType store_num)
 {
-    auto price_ask = price_item(player_ptr, o_ptr, ot_ptr->inflate, false, store_num);
+    auto price_ask = price_item(*player_ptr, o_ptr, ot_ptr->inflate, false, store_num);
 
     price_ask *= o_ptr->number;
     const auto s = fmt::format(_("買値 ${} で買いますか？", "Do you buy for ${}? "), price_ask);
@@ -192,7 +192,7 @@ void store_purchase(PlayerType *player_ptr, StoreSaleType store_num)
         return;
     }
 
-    const auto best = price_item(player_ptr, &item, ot_ptr->inflate, false, store_num);
+    const auto best = price_item(*player_ptr, &item, ot_ptr->inflate, false, store_num);
     if (item_store.number > 1) {
         if (store_num != StoreSaleType::HOME) {
             msg_format(_("一つにつき $%dです。", "That costs %d gold per item."), best);
