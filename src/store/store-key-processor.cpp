e81d7bd2adab86b9f81615c5fb4c067e8fcf6120
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-03-13 02:27:04 +0900

    refactor: cmd-io/cmd-lore公開関数をCreatureEntity&に変更

--- src/store/store-key-processor.cpp
+++ src/store/store-key-processor.cpp
@@ -186,7 +186,7 @@ void store_process_command(CreatureEntity &creature, StoreSaleType store_num)
         break;
     }
     case '/': {
-        do_cmd_query_symbol(player_ptr);
+        do_cmd_query_symbol(*player_ptr);
         break;
     }
     case 'C': {
