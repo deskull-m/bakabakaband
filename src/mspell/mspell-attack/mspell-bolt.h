48f267d447e57ae97fa9aec5f4adc6133cf5cb17
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-03-14 17:11:16 +0900

    refactor: mspell-attack/mspell-bolt公開関数をCreatureEntity&に変更

--- src/mspell/mspell-attack/mspell-bolt.h
+++ src/mspell/mspell-attack/mspell-bolt.h
@@ -9,11 +9,13 @@ class PlayerType;
 
 class MSpellBolt : public AbstractMSpellAttack {
 public:
-    MSpellBolt(PlayerType *player_ptr, MONSTER_IDX m_idx, MonsterAbilityType ability, int target_type);
-    MSpellBolt(PlayerType *player_ptr, MONSTER_IDX m_idx, MONSTER_IDX t_idx, MonsterAbilityType ability, int target_type);
+    MSpellBolt(CreatureEntity &creature, MONSTER_IDX m_idx, MonsterAbilityType ability, int target_type);
+    MSpellBolt(CreatureEntity &creature, MONSTER_IDX m_idx, MONSTER_IDX t_idx, MonsterAbilityType ability, int target_type);
     ~MSpellBolt() = default;
     MSpellBolt(const MSpellBolt &) = delete;
     MSpellBolt(MSpellBolt &&) = default;
     void operator=(const MSpellBolt &) = delete;
     MSpellBolt &operator=(MSpellBolt &&) = default;
 };
+
+class CreatureEntity;
