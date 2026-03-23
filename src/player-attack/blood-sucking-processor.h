5419a82bfce798a15e96319c6513aaa31d34857b
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-03-23 12:33:40 +0900

    refactor(player-attack): blood-sucking-processor.cppの各関数の第一引数をCreatureEntity&に変更し、player-attack.cppの不要なPlayerTypeキャストを除去

--- src/player-attack/blood-sucking-processor.h
+++ src/player-attack/blood-sucking-processor.h
@@ -1,8 +1,8 @@
 #pragma once
 
 struct player_attack_type;
-class PlayerType;
-void decide_blood_sucking(PlayerType *player_ptr, player_attack_type *pa_ptr);
-void decide_exorcism(PlayerType *player_ptr, player_attack_type *pa_ptr);
+class CreatureEntity;
+void decide_blood_sucking(CreatureEntity &creature, player_attack_type *pa_ptr);
+void decide_exorcism(CreatureEntity &creature, player_attack_type *pa_ptr);
 void calc_drain(player_attack_type *pa_ptr);
-void process_drain(PlayerType *player_ptr, player_attack_type *pa_ptr, const bool is_human, bool *drain_msg);
+void process_drain(CreatureEntity &creature, player_attack_type *pa_ptr, const bool is_human, bool *drain_msg);
