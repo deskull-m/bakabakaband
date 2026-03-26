8b9f32d26b711c1d2f73758eb1527d15b25d8b6c
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-03-26 12:25:37 +0900

    refactor(player): tim_player_flags の第一引数を PlayerType * から CreatureEntity & に変更

--- src/player/temporary-resistances.cpp
+++ src/player/temporary-resistances.cpp
@@ -9,8 +9,8 @@
 #include "realm/realm-types.h"
 #include "spell-realm/spells-hex.h"
 #include "status/element-resistance.h"
+#include "system/creature-entity.h"
 #include "system/item-entity.h"
-#include "system/player-type-definition.h"
 #include "util/bit-flags-calculator.h"
 
 /*!
@@ -19,7 +19,7 @@
  * @param player_ptr プレイヤーへの参照ポインタ
  * @param flags フラグを保管する配列
  */
-void tim_player_flags(PlayerType *player_ptr, TrFlags &flags)
+void tim_player_flags(CreatureEntity &creature, TrFlags &flags)
 {
     BIT_FLAGS tmp_effect_flag = FLAG_CAUSE_MAGIC_TIME_EFFECT;
     set_bits(tmp_effect_flag, FLAG_CAUSE_STANCE);
@@ -28,24 +28,24 @@ void tim_player_flags(PlayerType *player_ptr, TrFlags &flags)
 
     flags.clear();
 
-    if (is_oppose_acid(*player_ptr) && none_bits(has_immune_acid(*player_ptr), (race_class_flag | tmp_effect_flag))) {
+    if (is_oppose_acid(creature) && none_bits(has_immune_acid(creature), (race_class_flag | tmp_effect_flag))) {
         flags.set(TR_RES_ACID);
     }
-    if (is_oppose_elec(*player_ptr) && none_bits(has_immune_elec(*player_ptr), (race_class_flag | tmp_effect_flag))) {
+    if (is_oppose_elec(creature) && none_bits(has_immune_elec(creature), (race_class_flag | tmp_effect_flag))) {
         flags.set(TR_RES_ELEC);
     }
-    if (is_oppose_fire(*player_ptr) && none_bits(has_immune_fire(*player_ptr), (race_class_flag | tmp_effect_flag))) {
+    if (is_oppose_fire(creature) && none_bits(has_immune_fire(creature), (race_class_flag | tmp_effect_flag))) {
         flags.set(TR_RES_FIRE);
     }
-    if (is_oppose_cold(*player_ptr) && none_bits(has_immune_cold(*player_ptr), (race_class_flag | tmp_effect_flag))) {
+    if (is_oppose_cold(creature) && none_bits(has_immune_cold(creature), (race_class_flag | tmp_effect_flag))) {
         flags.set(TR_RES_COLD);
     }
-    if (is_oppose_pois(*player_ptr)) {
+    if (is_oppose_pois(creature)) {
         flags.set(TR_RES_POIS);
     }
 
     for (int test_flag = 0; test_flag < TR_FLAG_MAX; test_flag++) {
-        if (any_bits(get_player_flags(*player_ptr, i2enum<tr_type>(test_flag)), tmp_effect_flag)) {
+        if (any_bits(get_player_flags(creature, i2enum<tr_type>(test_flag)), tmp_effect_flag)) {
             flags.set(i2enum<tr_type>(test_flag));
         }
     }
