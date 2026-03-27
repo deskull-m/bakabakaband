5493c73004a3ff7a7129c801ea68465aea299d14
deskull <mailto:mailto:61610939+deskull-m@users.noreply.github.com>
2026-03-27 20:01:49 +0900

    refactor(world): WorldTurnProcessor の第一引数を PlayerType * から CreatureEntity & に変更

--- src/world/world-turn-processor.cpp
+++ src/world/world-turn-processor.cpp
@@ -30,6 +30,7 @@
 #include "store/store.h"
 #include "system/angband-system.h"
 #include "system/building-type-definition.h"
+#include "system/creature-entity.h"
 #include "system/dungeon/dungeon-definition.h"
 #include "system/enums/dungeon/dungeon-id.h"
 #include "system/floor/floor-info.h"
@@ -51,15 +52,14 @@
 #include "world/world.h"
 #include <range/v3/view.hpp>
 
-WorldTurnProcessor::WorldTurnProcessor(PlayerType *player_ptr)
-    : player_ptr(player_ptr)
+WorldTurnProcessor::WorldTurnProcessor(CreatureEntity &creature)
+    : creature(creature)
 {
 }
 
 /*!
  * @brief 10ゲームターンが進行する毎にゲーム世界全体の処理を行う。
  * / Handle certain things once every 10 game turns
- * @param player_ptr プレイヤーへの参照ポインタ
  */
 void WorldTurnProcessor::process_world()
 {
@@ -68,7 +68,7 @@ void WorldTurnProcessor::process_world()
     const int prev_turn_in_today = ((world.game_turn - TURNS_PER_TICK) % a_day + a_day / 4) % a_day;
     const int prev_min = (1440 * prev_turn_in_today / a_day) % 60;
     std::tie(std::ignore, this->hour, this->min) = world.extract_date_time(InnerGameData::get_instance().get_start_race());
-    update_dungeon_feeling(*this->player_ptr);
+    update_dungeon_feeling(this->creature);
     process_downward();
     process_monster_arena();
     if (world.game_turn % TURNS_PER_TICK) {
@@ -76,7 +76,7 @@ void WorldTurnProcessor::process_world()
     }
 
     decide_auto_save();
-    const auto &floor = *this->player_ptr->current_floor_ptr;
+    const auto &floor = *this->creature.current_floor_ptr;
     if (floor.monster_noise && !ignore_unview) {
         msg_print(_("何かが聞こえた。", "You hear noise."));
     }
@@ -86,26 +86,26 @@ void WorldTurnProcessor::process_world()
     if ((this->hour == 0) && (this->min == 0)) {
         if (this->min != prev_min) {
             exe_write_diary(floor, DiaryKind::DIALY, 0);
-            determine_daily_bounty(*this->player_ptr);
+            determine_daily_bounty(this->creature);
         }
     }
 
     ring_nightmare_bell(prev_min);
-    starve_player(*this->player_ptr);
-    process_player_hp_mp(*this->player_ptr);
-    reduce_magic_effects_timeout(*this->player_ptr);
-    reduce_lite_life(*this->player_ptr);
-    process_terrain_effects(*this->player_ptr);
-    process_world_aux_mutation(*this->player_ptr);
-    process_world_aux_sudden_attack(*this->player_ptr);
-    process_alliance_recovery(*this->player_ptr);
-    execute_cursed_items_effect(*this->player_ptr);
-    recharge_magic_items(*this->player_ptr);
-    sense_inventory1(*this->player_ptr);
-    sense_inventory2(*this->player_ptr);
-    execute_recall(*this->player_ptr);
-    execute_floor_reset(*this->player_ptr);
-    wc_ptr->plus_timed_world_collapsion(&world, *this->player_ptr, 10);
+    starve_player(this->creature);
+    process_player_hp_mp(this->creature);
+    reduce_magic_effects_timeout(this->creature);
+    reduce_lite_life(this->creature);
+    process_terrain_effects(this->creature);
+    process_world_aux_mutation(this->creature);
+    process_world_aux_sudden_attack(this->creature);
+    process_alliance_recovery(this->creature);
+    execute_cursed_items_effect(this->creature);
+    recharge_magic_items(this->creature);
+    sense_inventory1(this->creature);
+    sense_inventory2(this->creature);
+    execute_recall(this->creature);
+    execute_floor_reset(this->creature);
+    wc_ptr->plus_timed_world_collapsion(&world, this->creature, 10);
 }
 
 /*!
@@ -148,8 +148,9 @@ void WorldTurnProcessor::print_cheat_position()
     c_put_str(TERM_WHITE, "             ", ROW_NOW_POS, COL_NOW_POS);
     c_put_str(TERM_WHITE, "             ", ROW_OLD_POS, COL_OLD_POS);
     if (cheat_sight) {
+        auto *player_ptr = static_cast<PlayerType *>(&this->creature);
         c_put_str(TERM_WHITE,
-            format("nX:%03d nY:%03d", player_ptr->x, player_ptr->y), ROW_NOW_POS, COL_NOW_POS);
+            format("nX:%03d nY:%03d", this->creature.x, this->creature.y), ROW_NOW_POS, COL_NOW_POS);
         c_put_str(TERM_WHITE,
             format("oX:%03d oY:%03d", player_ptr->oldpx, player_ptr->oldpy), ROW_OLD_POS, COL_OLD_POS);
     }
@@ -158,7 +159,7 @@ void WorldTurnProcessor::print_cheat_position()
 void WorldTurnProcessor::process_downward()
 {
     /* 帰還無しモード時のレベルテレポバグ対策 / Fix for level teleport bugs on ironman_downward.*/
-    auto &floor = *this->player_ptr->current_floor_ptr;
+    auto &floor = *this->creature.current_floor_ptr;
     if (!ironman_downward || (floor.dungeon_id == DungeonId::ANGBAND) || !floor.is_underground() || floor.is_in_quest()) {
         return;
     }
@@ -168,16 +169,18 @@ void WorldTurnProcessor::process_downward()
     FloorChangeModesStore::get_instace()->set({ FloorChangeMode::FIRST_FLOOR, FloorChangeMode::RANDOM_PLACE });
     floor.inside_arena = false;
     AngbandWorld::get_instance().set_wild_mode(false);
-    this->player_ptr->leaving = true;
+    auto *player_ptr = static_cast<PlayerType *>(&this->creature);
+    player_ptr->leaving = true;
 }
 
 void WorldTurnProcessor::process_monster_arena()
 {
-    if (!AngbandSystem::get_instance().is_phase_out() || this->player_ptr->leaving) {
+    auto *player_ptr = static_cast<PlayerType *>(&this->creature);
+    if (!AngbandSystem::get_instance().is_phase_out() || player_ptr->leaving) {
         return;
     }
 
-    const auto &floor = *this->player_ptr->current_floor_ptr;
+    const auto &floor = *this->creature.current_floor_ptr;
     const auto monster_exists = [&](const Pos2D &pos) {
         const auto &grid = floor.get_grid(pos);
         return grid.has_monster() && !floor.m_list[grid.m_idx].is_riding();
@@ -191,9 +194,9 @@ void WorldTurnProcessor::process_monster_arena()
     if (m_idxs.empty()) {
         msg_print(_("相打ちに終わりました。", "Nothing survived."));
         msg_erase();
-        this->player_ptr->energy_need = 0;
+        player_ptr->energy_need = 0;
         auto &melee_arena = MeleeArena::get_instance();
-        melee_arena.update_gladiators(*this->player_ptr);
+        melee_arena.update_gladiators(this->creature);
         return;
     }
 
@@ -229,17 +232,18 @@ void WorldTurnProcessor::process_monster_arena_winner(int win_m_idx)
 
 void WorldTurnProcessor::process_monster_arena_draw()
 {
-    auto turn = this->player_ptr->current_floor_ptr->generated_turn;
+    auto turn = this->creature.current_floor_ptr->generated_turn;
     if (AngbandWorld::get_instance().game_turn - turn != 150 * TURNS_PER_TICK) {
         return;
     }
 
+    auto *player_ptr = static_cast<PlayerType *>(&this->creature);
     msg_print(_("申し訳ありませんが、この勝負は引き分けとさせていただきます。", "Sorry, but this battle ended in a draw."));
-    this->player_ptr->au += MeleeArena::get_instance().get_payback(true);
+    player_ptr->au += MeleeArena::get_instance().get_payback(true);
     msg_erase();
-    this->player_ptr->energy_need = 0;
+    player_ptr->energy_need = 0;
     auto &melee_arena = MeleeArena::get_instance();
-    melee_arena.update_gladiators(*this->player_ptr);
+    melee_arena.update_gladiators(this->creature);
 }
 
 void WorldTurnProcessor::decide_auto_save()
@@ -252,21 +256,21 @@ void WorldTurnProcessor::decide_auto_save()
     should_save &= !AngbandSystem::get_instance().is_phase_out();
     should_save &= AngbandWorld::get_instance().game_turn % ((int32_t)autosave_freq * TURNS_PER_TICK) == 0;
     if (should_save) {
-        do_cmd_save_game(*this->player_ptr, true);
+        do_cmd_save_game(this->creature, true);
     }
 }
 
 void WorldTurnProcessor::process_change_daytime_night()
 {
-    const auto &floor = *this->player_ptr->current_floor_ptr;
+    const auto &floor = *this->creature.current_floor_ptr;
     const auto &world = AngbandWorld::get_instance();
     if (!floor.is_underground() && !floor.is_in_quest() && !AngbandSystem::get_instance().is_phase_out() && !floor.inside_arena) {
         if (!(world.game_turn % ((TURNS_PER_TICK * TOWN_DAWN) / 2))) {
             auto dawn = world.game_turn % (TURNS_PER_TICK * TOWN_DAWN) == 0;
             if (dawn) {
-                day_break(*this->player_ptr);
+                day_break(this->creature);
             } else {
-                night_falls(*this->player_ptr);
+                night_falls(this->creature);
             }
         }
 
@@ -290,33 +294,34 @@ void WorldTurnProcessor::process_world_monsters()
     decide_alloc_monster();
     const auto &world = AngbandWorld::get_instance();
     if (!(world.game_turn % (TURNS_PER_TICK * 10)) && !AngbandSystem::get_instance().is_phase_out()) {
-        regenerate_monsters(*this->player_ptr);
+        regenerate_monsters(this->creature);
     }
 
     if (!(world.game_turn % (TURNS_PER_TICK * 3))) {
-        regenerate_captured_monsters(*this->player_ptr);
+        regenerate_captured_monsters(this->creature);
     }
 
-    if (this->player_ptr->leaving) {
+    auto *player_ptr = static_cast<PlayerType *>(&this->creature);
+    if (player_ptr->leaving) {
         return;
     }
 
     for (const auto mte : MONSTER_TIMED_EFFECT_RANGE) {
-        if (this->player_ptr->current_floor_ptr->mproc_max[mte] > 0) {
-            process_monsters_mtimed(*this->player_ptr, mte);
+        if (this->creature.current_floor_ptr->mproc_max[mte] > 0) {
+            process_monsters_mtimed(this->creature, mte);
         }
     }
 }
 
 void WorldTurnProcessor::decide_alloc_monster()
 {
-    const auto &floor = *this->player_ptr->current_floor_ptr;
+    const auto &floor = *this->creature.current_floor_ptr;
     auto should_alloc = one_in_(floor.get_dungeon_definition().max_m_alloc_chance);
     should_alloc &= !floor.inside_arena;
     should_alloc &= !floor.is_in_quest();
     should_alloc &= !AngbandSystem::get_instance().is_phase_out();
     if (should_alloc) {
-        (void)alloc_monster(*this->player_ptr, MAX_PLAYER_SIGHT + 5, 0, summon_specific);
+        (void)alloc_monster(this->creature, MAX_PLAYER_SIGHT + 5, 0, summon_specific);
     }
 }
 
@@ -331,7 +336,7 @@ void WorldTurnProcessor::ring_nightmare_bell(int prev_min)
     }
 
     if ((this->hour == 23) && !(this->min % 15)) {
-        disturb(*this->player_ptr, false, true);
+        disturb(this->creature, false, true);
         switch (this->min / 15) {
         case 0:
             msg_print(_("遠くで不気味な鐘の音が鳴った。", "You hear a distant bell toll ominously."));
@@ -355,14 +360,15 @@ void WorldTurnProcessor::ring_nightmare_bell(int prev_min)
         return;
     }
 
-    disturb(*this->player_ptr, true, true);
+    auto *player_ptr = static_cast<PlayerType *>(&this->creature);
+    disturb(this->creature, true, true);
     msg_print(_("遠くで鐘が何回も鳴り、死んだような静けさの中へ消えていった。", "A distant bell tolls many times, fading into an deathly silence."));
     if (AngbandWorld::get_instance().is_wild_mode()) {
-        this->player_ptr->oldpy = randint1(MAX_HGT - 2);
-        this->player_ptr->oldpx = randint1(MAX_WID - 2);
-        change_wild_mode(*this->player_ptr, true);
-        PlayerEnergy(*this->player_ptr).set_player_turn_energy(100);
+        player_ptr->oldpy = randint1(MAX_HGT - 2);
+        player_ptr->oldpx = randint1(MAX_WID - 2);
+        change_wild_mode(this->creature, true);
+        PlayerEnergy(this->creature).set_player_turn_energy(100);
     }
 
-    this->player_ptr->invoking_midnight_curse = true;
+    player_ptr->invoking_midnight_curse = true;
 }
